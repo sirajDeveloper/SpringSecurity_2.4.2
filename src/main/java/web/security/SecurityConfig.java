64c1135d851582dcb75fa0bbc051df861448301a
siraj_dev <y-taksa@yandex.ru>
2020-07-06 20:18:31 +0500

    seventh commit

--- src/main/java/web/security/SecurityConfig.java
+++ src/main/java/web/security/SecurityConfig.java
@@ -32,40 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/login").authenticated()
+                .authorizeRequests()
                 .antMatchers("/admin/**").access("hasAnyRole('ADMIN')")
                 .antMatchers("/user/**").access("hasAnyRole('USER')")
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .successHandler(new LoginSuccessHandler())
                 .and().logout();
-        /*http.formLogin()
-                // указываем страницу с формой логина
-                .loginPage("/login")
-                //указываем логику обработки при логине
-                .successHandler(new LoginSuccessHandler())
-                // указываем action с формы логина
-                .loginProcessingUrl("/login")
-                // Указываем параметры логина и пароля с формы логина
-                .usernameParameter("username")
-                .passwordParameter("password")
-                // даем доступ к форме логина всем
-                .permitAll();
-
-        http.logout()
-                .logoutSuccessUrl("/login?logout")
-                //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
-                .and().csrf().disable();
-
-        http
-                // делаем страницу регистрации недоступной для авторизированных пользователей
-                .authorizeRequests()
-                //страницы аутентификаци доступна всем
-                .antMatchers("/login").anonymous()
-                // защищенные URL
-                .antMatchers("/admin").access("hasAnyRole('ADMIN')")
-                .antMatchers("/user").access("hasAnyRole('USER')")
-                .anyRequest().authenticated();*/
     }
 
     @Bean
